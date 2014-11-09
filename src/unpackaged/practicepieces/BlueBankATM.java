64e1a34620214a9f28b06e14b811a276eaf371fd
Arie <arie@igroup.co.za>
2014-11-09 17:40:57 +0200

    Hacked a fix for the BlueBankATM call to get money

--- src/unpackaged/practicepieces/BlueBankATM.java
+++ src/unpackaged/practicepieces/BlueBankATM.java
@@ -1,22 +1,25 @@
 public class BlueBankATM{
-	public BlueBankATM(){};
-	protected BlueBankATM(String args[]) {
-		
+
+	private BlueBankATM(String args[]) {
+	
 		BlueBankRequest b1 = new BlueBankRequest();
-		b1.setPin(args[0]);
-		//b1.setPin("9010");
+		//b1.setPin(args[0]);
+		System.out.println("PIN is: "+ b1.getPin());
+		b1.setPin("9010");
 		RedBankConnection redBankConnection = RedBank.getRedBankConnection();
 		boolean pinOk = redBankConnection.isPinOk(b1);
 		if (pinOk) {
-			System.out.println("Here your monies! Haz a cheezburger! Your PIN was " + args[0]);
+			System.out.println("Here your monies! Haz a cheezburger!");
 		} else {
-			System.out.println("You haz a sad. Yes you do. Your PIN was " + args[0]);
+			System.out.println("You haz a sad. Yes you do.");
 		}
 		
 	}
 	
 	public static void main (String args[]) {
-      new BlueBankATM(args);
+	    //Supply PIN through main class?
+	    //String mypin = "9010";
+	    new BlueBankATM(args);
 
     }
  }
