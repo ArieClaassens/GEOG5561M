c6ee53665d581451ff4ca458e1a5eecd3ccbd6c2
Arie <arieclaassens@gmail.com>
2014-11-15 15:34:33 +0200

    Tested PMD

--- src/unpackaged/framework5/HelloFrame.java
+++ src/unpackaged/framework5/HelloFrame.java
@@ -3,16 +3,16 @@ import java.awt.Label;
 
 public class HelloFrame extends JFrame {
 
-   public HelloFrame () {
-      super("HelloWorld!");
-      setSize(300,300);
-      setLocation(300,300);
-      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      add(new Label("Hello World, I am here!"));
-      setVisible(true);
-   }
+    public HelloFrame() {
+        super("HelloWorld!");
+        setSize(300, 300);
+        setLocation(300, 300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        add(new Label("Hello World, I am here!"));
+        setVisible(true);
+    }
 
-   public static void main (String args[]) {
-      new HelloFrame();
-   }
+    public static void main(String args[]) {
+        new HelloFrame();
+    }
 }
